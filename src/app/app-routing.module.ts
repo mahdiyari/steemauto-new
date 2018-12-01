698dc1259c3455bda37f47cdadd70c23669987df
Mahdi Yari <m.yary15@gmail.com>
2018-12-01 18:23:29 +0330

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './home/home.component'
+import { NotFoundComponent } from './not-found/not-found.component'
+import { AboutComponent } from './about/about.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  // { path: 'about', component: AboutComponent, outlet: 'two' },
+  // { path: 'path/:routeParam', component: HomeComponent },
+  // { path: 'staticPath', component: HomeComponent },
+  { path: '**', component: NotFoundComponent },
+  // { path: 'oldPath', redirectTo: '/staticPath' },
+  // { path: ..., component: ..., data: { message: 'Custom' } }
+  ]
+
+  const routing = RouterModule.forRoot(routes)
+
 
 @NgModule({
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  imports: [ routing ]
 })
 export class AppRoutingModule {}
