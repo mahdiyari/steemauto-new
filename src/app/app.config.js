2386a7c462af45b45a30ae60a807f2c202ae4208
Mahdi Yari <m.yary15@gmail.com>
2018-12-18 19:52:55 +0330

    api urls added

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -9,7 +9,16 @@ const config = new function () {
   }
   this.api = {}
   this.api.url = 'http://localhost:3001'
-  this.api.login_api = `${this.api.url}/api/v1/login`
+  this.api.login_api = this.api.url + '/api/v1/login'
+  this.api.price = this.api.url + '/api/v1.1/dashboard/price'
+  this.api.stats = this.api.url + '/api/v1.1/dashboard/stats'
+  this.api.set_limits = this.api.url + '/api/v1.1/dashboard/set_limits'
+  this.api.get_limits = this.api.url + '/api/v1.1/dashboard/get_limits'
+  this.api.successful_votes = this.api.url + '/api/v1.1/dashboard/successful_votes'
+  this.api.failed_votes = this.api.url + '/api/v1.1/dashboard/failed_votes'
+  this.api.following_trails = this.api.url + '/api/v1.1/curation-trail/following'
+  this.api.top_trails = this.api.url + '/api/v1.1/curation-trail/top_trails'
+  this.api.search_trail = this.api.url + '/api/v1.1/curation-trail/search'
   this.rpc = {
     https: 'https://api.steemit.com'
   }
