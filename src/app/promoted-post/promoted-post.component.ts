cccef8e45e7c5b12f439d4abbcad74457e97834a
Mahdi Yari <m.yary15@gmail.com>
2018-12-18 19:56:57 +0330

    promoted posts section(Ads)

---
+++ src/app/promoted-post/promoted-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-promoted-post',
+  templateUrl: './promoted-post.component.html',
+  styleUrls: ['./promoted-post.component.css']
+})
+export class PromotedPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
