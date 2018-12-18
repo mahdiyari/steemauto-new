a258ef58bf5df9fb5250f04fc38dc70cbfa017fa
Mahdi Yari <m.yary15@gmail.com>
2018-12-18 19:58:49 +0330

    curation-trail added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component'
 import { NotFoundComponent } from './not-found/not-found.component'
 import { AboutComponent } from './about/about.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { CurationTrailComponent } from './curation-trail/curation-trail.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'curation-trail', component: CurationTrailComponent },
   // { path: 'about', component: AboutComponent, outlet: 'two' },
   // { path: 'path/:routeParam', component: HomeComponent },
   // { path: 'staticPath', component: HomeComponent },
