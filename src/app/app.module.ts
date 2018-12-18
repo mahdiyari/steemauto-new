30a87bed9c3984a7073d683ecca621ac83bef94b
Mahdi Yari <m.yary15@gmail.com>
2018-12-18 19:58:41 +0330

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,23 @@ import { HomeComponent } from './home/home.component'
 import { NotFoundComponent } from './not-found/not-found.component'
 import { HeaderComponent } from './header/header.component'
 import { FooterComponent } from './footer/footer.component'
-import { AnnouncementComponent } from './announcement/announcement.component'
 import { AboutComponent } from './about/about.component'
 import { HttpClientModule } from '@angular/common/http'
-import { SidebarComponent } from './header/sidebar/sidebar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LimitsComponent } from './dashboard/limits/limits.component';
-import { BalanceComponent } from './dashboard/balance/balance.component';
+import { SidebarComponent } from './header/sidebar/sidebar.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { LimitsComponent } from './dashboard/limits/limits.component'
+import { BalanceComponent } from './dashboard/balance/balance.component'
 import { StatsComponent } from './dashboard/stats/stats.component'
+import { PriceComponent } from './dashboard/price/price.component'
+import { VotingComponent } from './dashboard/voting-detail/voting-detail.component'
+import { TimeAgoPipe } from 'time-ago-pipe'
+import { VoteHistoryComponent } from './dashboard/vote-history/vote-history.component';
+import { AnnouncementComponent } from './dashboard/announcement/announcement.component';
+import { CurationTrailComponent } from './curation-trail/curation-trail.component';
+import { PromotedPostComponent } from './promoted-post/promoted-post.component';
+import { FollowingTrailsComponent } from './curation-trail/following-trails/following-trails.component';
+import { TopTrailsComponent } from './curation-trail/top-trails/top-trails.component';
+import { SeaarchTrailComponent } from './curation-trail/seaarch-trail/seaarch-trail.component'
 
 @NgModule({
   declarations: [
@@ -23,20 +32,25 @@ import { StatsComponent } from './dashboard/stats/stats.component'
     NotFoundComponent,
     HeaderComponent,
     FooterComponent,
-    AnnouncementComponent,
     AboutComponent,
     SidebarComponent,
     DashboardComponent,
     LimitsComponent,
     BalanceComponent,
-    StatsComponent
+    StatsComponent,
+    PriceComponent,
+    VotingComponent,
+    TimeAgoPipe,
+    VoteHistoryComponent,
+    AnnouncementComponent,
+    CurationTrailComponent,
+    PromotedPostComponent,
+    FollowingTrailsComponent,
+    TopTrailsComponent,
+    SeaarchTrailComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
