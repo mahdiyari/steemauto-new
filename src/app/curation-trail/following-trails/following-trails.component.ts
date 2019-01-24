aa97e499278d0b182db2f2a8256ff20167ad8d9c
Mahdi Yari <m.yary15@gmail.com>
2019-01-24 13:48:06 +0330

    sort + export methods to the parent class

--- src/app/curation-trail/following-trails/following-trails.component.ts
+++ src/app/curation-trail/following-trails/following-trails.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { CurationTrailComponent } from '../curation-trail.component'
 import { HttpClient } from '@angular/common/http'
-import config from '../../app.config'
 import { AuthService } from 'src/app/services/auth.service'
+import { sort } from '../../utils/sort'
 
 @Component({
   selector: 'app-following-trails',
@@ -10,24 +10,10 @@ import { AuthService } from 'src/app/services/auth.service'
   styleUrls: ['./following-trails.component.css']
 })
 export class FollowingTrailsComponent implements OnInit {
-  public followingTrails = []
-  public followingPage = {page: 1}
-  constructor(public _t: CurationTrailComponent, public _http: HttpClient, public auth: AuthService) {
-    this.getFollowing()
-  }
+  public sort = sort
 
-  public getFollowing() {
-    this._http
-      .post(config.api.following_trails, {
-        user: this.auth.username
-      })
-      .toPromise()
-      .then(res => {
-        if (res['id'] === 1) {
-          this.followingTrails = res['result']
-        }
-      })
-  }
+  constructor(public _http: HttpClient, public auth: AuthService, public _t: CurationTrailComponent) {}
+
   ngOnInit() {
   }
 
